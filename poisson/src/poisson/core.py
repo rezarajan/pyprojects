dadaef64b853c7c91c3cb4854d7fbc00c12e7c78
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2025-08-18 13:55:55 +0000

    (format) improve line spacing in poisson lib

--- poisson/src/poisson/core.py
+++ poisson/src/poisson/core.py
@@ -1,12 +1,14 @@
 # Poisson Calculator Helper Functions
 from math import pow, factorial, e
 
+
 def poisson(lam: float, k: int) -> float:
     """Returns the probability of obtaining k successes in a poisson experiment.
     lam: average number of successes in a fixed time interval
     k: number of successes
     """
     return pow(lam, k) * pow(e, -lam) / factorial(k)
+
 
 def lt_poisson(lam: float, k: int) -> float:
     """Returns the probability of obtaining *fewer than k* successes in a poisson experiment.
@@ -20,13 +22,15 @@ def lt_poisson(lam: float, k: int) -> float:
 
     return p
 
+
 def gt_poisson(lam: float, k: int) -> float:
     """Returns the probability of obtaining *more than k* successes in a poisson experiment.
     lam: average number of successes in a fixed time interval
     k: number of successes
     """
 
-    return 1 - lt_poisson(lam, k+1)
+    return 1 - lt_poisson(lam, k + 1)
+
 
 def leq_poisson(lam: float, k: int) -> float:
     """Returns the probability of obtaining *k or less* successes in a poisson experiment.
@@ -34,7 +38,8 @@ def leq_poisson(lam: float, k: int) -> float:
     k: number of successes
     """
 
-    return lt_poisson(lam, k+1)
+    return lt_poisson(lam, k + 1)
+
 
 def geq_poisson(lam: float, k: int) -> float:
     """Returns the probability of obtaining *k or more* successes in a poisson experiment.
--- poisson/tests/test_poisson.py
+++ poisson/tests/test_poisson.py
@@ -4,25 +4,31 @@ from poisson.core import poisson, lt_poisson, gt_poisson, leq_poisson, geq_poiss
 # Tolerance for floating-point comparisons (5 decimal places)
 TOL = 1e-4
 
+
 def isclose(a: float, b: float) -> float:
     """Shadows math.isclose with the included tolerance"""
     return _isclose(a, b, rel_tol=TOL)
+
 
 def test_poisson():
     assert isclose(poisson(5, 3), 0.14037)
     assert isclose(poisson(10, 5), 0.03783)
 
+
 def test_lt_poisson():
     assert isclose(lt_poisson(5, 3), 0.12465)
     assert isclose(lt_poisson(10, 5), 0.02925)
+
 
 def test_gt_poisson():
     assert isclose(gt_poisson(5, 3), 0.73497)
     assert isclose(gt_poisson(10, 5), 0.93291)
 
+
 def test_leq_poisson():
     assert isclose(leq_poisson(5, 3), 0.26503)
     assert isclose(leq_poisson(10, 5), 0.06709)
+
 
 def test_geq_poisson():
     assert isclose(geq_poisson(5, 3), 0.87535)
